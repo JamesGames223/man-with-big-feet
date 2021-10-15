44d4c87da7625165658da713c3605a6fa9b55a3c
JamesGames223 <92526733+JamesGames223@users.noreply.github.com>
2021-10-15 13:35:21 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,100 +1,322 @@
 function Character_Animations () {
     characterAnimations.loopFrames(
-    mySprite,
+    player_1,
     [img`
         . . . . . . f f f f . . . . . . 
-        . . . . f f f 2 2 f f f . . . . 
-        . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f e e e e e e f f f . . 
-        . . f f e 2 2 2 2 2 2 e e f . . 
-        . . f e 2 f f f f f f 2 e f . . 
+        . . . . . f f 6 6 f f . . . . . 
+        . . . . f f 6 6 6 6 f f . . . . 
+        . . . f f 8 8 8 8 8 8 f f . . . 
+        . . f f 8 6 6 6 6 6 6 8 f f . . 
+        . . f 8 6 f f f f f f 6 8 f . . 
         . . f f f f e e e e f f f f . . 
         . f f e f b f 4 4 f b f e f f . 
         . f e e 4 1 f d d f 1 4 e e f . 
         . . f e e d d d d d d e e f . . 
         . . . f e e 4 4 4 4 e e f . . . 
-        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . e 4 f 8 8 8 8 8 8 f 4 e . . 
+        . . 4 d f 6 6 6 6 6 6 f d 4 . . 
+        . . 4 4 f 8 8 8 8 8 8 f 4 4 . . 
         . . . . . f f f f f f . . . . . 
         . . . . . f f . . f f . . . . . 
         `,img`
         . . . . . . . . . . . . . . . . 
         . . . . . . f f f f . . . . . . 
-        . . . . f f f 2 2 f f f . . . . 
-        . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f e e e e e e f f f . . 
-        . . f f e 2 2 2 2 2 2 e e f . . 
-        . f f e 2 f f f f f f 2 e f f . 
+        . . . . . f f 6 6 f f . . . . . 
+        . . . . f f 6 6 6 6 f f . . . . 
+        . . . f f 8 8 8 8 8 8 f f . . . 
+        . . f f 8 6 6 6 6 6 6 8 f f . . 
+        . f f 8 6 f f f f f f 6 8 f f . 
         . f f f f f e e e e f f f f f . 
         . . f e f b f 4 4 f b f e f . . 
         . . f e 4 1 f d d f 1 4 e f . . 
         . . . f e 4 d d d d 4 e f e . . 
-        . . f e f 2 2 2 2 e d d 4 e . . 
-        . . e 4 f 2 2 2 2 e d d e . . . 
-        . . . . f 4 4 5 5 f e e . . . . 
+        . . f e f 8 8 8 8 e d d 4 e . . 
+        . . e 4 f 6 6 6 6 e d d e . . . 
+        . . . . f 8 8 8 8 8 e e . . . . 
         . . . . f f f f f f f . . . . . 
         . . . . f f f . . . . . . . . . 
         `,img`
         . . . . . . f f f f . . . . . . 
-        . . . . f f f 2 2 f f f . . . . 
-        . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f e e e e e e f f f . . 
-        . . f f e 2 2 2 2 2 2 e e f . . 
-        . . f e 2 f f f f f f 2 e f . . 
+        . . . . . f f 6 6 f f . . . . . 
+        . . . . f f 6 6 6 6 f f . . . . 
+        . . . f f 8 8 8 8 8 8 f f . . . 
+        . . f f 8 6 6 6 6 6 6 8 f f . . 
+        . . f 8 6 f f f f f f 6 8 f . . 
         . . f f f f e e e e f f f f . . 
         . f f e f b f 4 4 f b f e f f . 
         . f e e 4 1 f d d f 1 4 e e f . 
         . . f e e d d d d d d e e f . . 
         . . . f e e 4 4 4 4 e e f . . . 
-        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . e 4 f 8 8 8 8 8 8 f 4 e . . 
+        . . 4 d f 6 6 6 6 6 6 f d 4 . . 
+        . . 4 4 f 8 8 8 8 8 8 f 4 4 . . 
         . . . . . f f f f f f . . . . . 
         . . . . . f f . . f f . . . . . 
         `,img`
         . . . . . . . . . . . . . . . . 
         . . . . . . f f f f . . . . . . 
-        . . . . f f f 2 2 f f f . . . . 
-        . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f e e e e e e f f f . . 
-        . . f e e 2 2 2 2 2 2 e f f . . 
-        . f f e 2 f f f f f f 2 e f f . 
+        . . . . . f f 6 6 f f . . . . . 
+        . . . . f f 6 6 6 6 f f . . . . 
+        . . . f f 8 8 8 8 8 8 f f . . . 
+        . . f f 8 6 6 6 6 6 6 8 f f . . 
+        . f f 8 6 f f f f f f 6 8 f f . 
         . f f f f f e e e e f f f f f . 
         . . f e f b f 4 4 f b f e f . . 
         . . f e 4 1 f d d f 1 4 e f . . 
         . . e f e 4 d d d d 4 e f . . . 
-        . . e 4 d d e 2 2 2 2 f e f . . 
-        . . . e d d e 2 2 2 2 f 4 e . . 
-        . . . . e e f 5 5 4 4 f . . . . 
+        . . e 4 d d e 8 8 8 8 f e f . . 
+        . . . e d d e 6 6 6 6 f 4 e . . 
+        . . . . e e f 8 8 8 8 f . . . . 
         . . . . . f f f f f f f . . . . 
         . . . . . . . . . f f f . . . . 
         `],
-    500,
+    200,
     characterAnimations.rule(Predicate.MovingDown)
     )
+    characterAnimations.loopFrames(
+    player_1,
+    [img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . f f f f f 6 6 f f f f f . . 
+        . . f f e 6 e 6 6 e 6 e f f . . 
+        . . f e 6 f 6 f f 6 f 6 e f . . 
+        . . f f f 6 6 e e 6 6 f f f . . 
+        . f f e f 6 f e e f 6 f e f f . 
+        . f e e f f e e e e f e e e f . 
+        . . f e e e e e e e e e e f . . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f 4 e . . 
+        . . 4 d f 6 6 6 6 6 6 f d 4 . . 
+        . . 4 4 f 8 8 8 8 8 8 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . . f f f f 6 6 f f f f . . . 
+        . . f f e 6 e 6 6 e 6 e f f . . 
+        . . f e 6 f 6 f f f 6 f e f . . 
+        . . f f f 6 f e e 6 6 f f f . . 
+        . . f e 6 f f e e 6 f e e f . . 
+        . f f e f f e e e f e e e f f . 
+        . f f e e e e e e e e e e f f . 
+        . . . f e e e e e e e e f . . . 
+        . . . e f f f f f f f f 4 e . . 
+        . . . 4 f 6 6 6 6 6 e d d 4 . . 
+        . . . e f f f f f f e e 4 . . . 
+        . . . . f f f . . . . . . . . . 
+        `,img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . f f f f f 6 6 f f f f f . . 
+        . . f f e 6 e 6 6 e 6 e f f . . 
+        . . f e 6 f 6 f f 6 f 6 e f . . 
+        . . f f f 6 6 e e 6 6 f f f . . 
+        . f f e f 6 f e e f 6 f e f f . 
+        . f e e f f e e e e f e e e f . 
+        . . f e e e e e e e e e e f . . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f 4 e . . 
+        . . 4 d f 6 6 6 6 6 6 f d 4 . . 
+        . . 4 4 f 8 8 8 8 8 8 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . . f f f f 6 6 f f f f . . . 
+        . . f f e 6 e 6 6 e 6 e f f . . 
+        . . f e f 6 f f f 6 f 6 e f . . 
+        . . f f f 6 6 e e f 6 f f f . . 
+        . . f e e f 6 e e f f 6 e f . . 
+        . f f e e e f e e e f f e f f . 
+        . f f e e e e e e e e e e f f . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f e . . . 
+        . . 4 d d e 6 6 6 6 6 f 4 . . . 
+        . . . 4 e e f f f f f f e . . . 
+        . . . . . . . . . f f f . . . . 
+        `],
+    200,
+    characterAnimations.rule(Predicate.MovingUp)
+    )
+    characterAnimations.loopFrames(
+    player_1,
+    [img`
+        . . . . f f f f f f . . . . . . 
+        . . . f 6 f e e e e f f . . . . 
+        . . f 6 6 6 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 6 6 6 6 e e f f f f . . . 
+        . f 6 e f f f f 6 6 6 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d d 4 e e e f . . . 
+        . . . f e 4 4 4 e e f f . . . . 
+        . . . f 8 8 8 e d d 4 . . . . . 
+        . . . f 6 6 6 e d d e . . . . . 
+        . . . f 8 8 8 f e e f . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . f 6 f e e e e f f . . . . 
+        . . f 6 6 6 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 6 6 6 6 e e f f f f . . . 
+        . f 6 e f f f f 6 6 6 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d e e e e e f . . . 
+        . . . f 8 8 e d d 4 f . . . . . 
+        . . . f 6 6 e d d e f . . . . . 
+        . f f f 8 8 f e e f f f . . . . 
+        . . f f f f f f f f f f . . . . 
+        . . . f f f . . . f f . . . . . 
+        `,img`
+        . . . . f f f f f f . . . . . . 
+        . . . f 6 f e e e e f f . . . . 
+        . . f 6 6 6 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 6 6 6 6 e e f f f f . . . 
+        . f 6 e f f f f 6 6 6 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d d 4 e e e f . . . 
+        . . . f e 4 4 4 e e f f . . . . 
+        . . . f 8 8 8 e d d 4 . . . . . 
+        . . . f 6 6 6 e d d e . . . . . 
+        . . . f 8 8 8 f e e f . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . f 6 f e e e e f f . . . . 
+        . . f 6 6 6 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 6 6 6 6 e e f f f f . . . 
+        . f 6 e f f f f 6 6 6 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d d 4 e e e f . . . 
+        . . . f 8 8 8 8 e d d 4 . . . . 
+        . . . f 6 6 6 6 e d d e . . . . 
+        . . f f 8 8 8 8 f e e f . . . . 
+        . . f f f f f f f f f f . . . . 
+        . . . f f f . . . f f . . . . . 
+        `],
+    200,
+    characterAnimations.rule(Predicate.MovingLeft)
+    )
+    characterAnimations.loopFrames(
+    player_1,
+    [img`
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 6 6 6 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 6 6 6 6 e f . 
+        . . . f e 6 6 6 f f f f e 6 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e 4 d d d d f . . . 
+        . . . . f f e e 4 4 4 e f . . . 
+        . . . . . 4 d d e 8 8 8 f . . . 
+        . . . . . e d d e 6 6 6 f . . . 
+        . . . . . f e e f 8 8 8 f . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 6 f . . . 
+        . . . f f e e e e f 6 6 6 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 6 6 6 6 e f . 
+        . . . f e 6 6 6 f f f f e 6 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e e e d d d f . . . 
+        . . . . . f 4 d d e 4 e f . . . 
+        . . . . . f e d d e 6 6 f . . . 
+        . . . . f f f e e f 8 8 f f . . 
+        . . . . f f f f f f f f f f . . 
+        . . . . . f f . . . f f f . . . 
+        `,img`
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 6 f . . . 
+        . . . f f e e e e f 6 6 6 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 6 6 6 6 e f . 
+        . . . f e 6 6 6 f f f f e 6 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e 4 d d d d f . . . 
+        . . . . f f e e 4 4 4 e f . . . 
+        . . . . . 4 d d e 8 8 8 f . . . 
+        . . . . . e d d e 6 6 6 f . . . 
+        . . . . . f e e f 8 8 8 f . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 6 f . . . 
+        . . . f f e e e e f 6 6 6 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 6 6 6 6 e f . 
+        . . . f e 6 6 6 f f f f e 6 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e 4 d d d d f . . . 
+        . . . . 4 d d e 4 4 4 e f . . . 
+        . . . . e d d e 6 6 6 6 f . . . 
+        . . . . f e e f 8 8 8 8 f f . . 
+        . . . . f f f f f f f f f f . . 
+        . . . . . f f . . . f f f . . . 
+        `],
+    200,
+    characterAnimations.rule(Predicate.MovingRight)
+    )
 }
-let mySprite: Sprite = null
-mySprite = sprites.create(img`
+let player_1: Sprite = null
+player_1 = sprites.create(img`
     . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
+    . . . . . f f 6 6 f f . . . . . 
+    . . . . f f 6 6 6 6 f f . . . . 
+    . . . f f 8 8 8 8 8 8 f f . . . 
+    . . f f 8 6 6 6 6 6 6 8 f f . . 
+    . . f 8 6 f f f f f f 6 8 f . . 
     . . f f f f e e e e f f f f . . 
     . f f e f b f 4 4 f b f e f f . 
     . f e e 4 1 f d d f 1 4 e e f . 
     . . f e e d d d d d d e e f . . 
     . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . e 4 f 8 8 8 8 8 8 f 4 e . . 
+    . . 4 d f 6 6 6 6 6 6 f d 4 . . 
+    . . 4 4 f 8 8 8 8 8 8 f 4 4 . . 
     . . . . . f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
-tiles.setTilemap(tilemap`level1`)
-scene.cameraFollowSprite(mySprite)
+controller.moveSprite(player_1, 100, 100)
+tiles.setTilemap(tilemap`level2`)
+scene.cameraFollowSprite(player_1)
+tiles.placeOnRandomTile(player_1, sprites.dungeon.floorLight2)
 Character_Animations()
-tiles.placeOnRandomTile(mySprite, sprites.castle.tileGrass2)
-controller.moveSprite(mySprite)
